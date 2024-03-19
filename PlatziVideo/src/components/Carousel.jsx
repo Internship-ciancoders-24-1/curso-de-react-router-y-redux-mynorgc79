776b5743f801729f0a71aef22d72b467f7399d47
mynorgc79 <mynorguarchaj99@gmail.com>
2024-03-19 13:37:11 -0600

    C-r-rr, capitulo 4 del curso de react router y redux

--- PlatziVideo/src/components/Carousel.jsx
+++ PlatziVideo/src/components/Carousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/styles/components/Carousel.scss';
 
+
 const Carousel = ({ children }) => (
   <section className="carousel">
     <div className="carousel__container">
